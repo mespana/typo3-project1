1aadbfa19a0bb28bb87a4bc992fc8f304f06d1c8
Mariana España-Rivera <marianaespana@yahoo.es>
2021-11-10 17:27:21 +0100

    update deploy

--- deploy.php
+++ deploy.php
@@ -25,8 +25,8 @@ set('use_relative_symlink', '0');
 
 // Hosts
 
-host('ftp109730-2622751@marianaespana.com')
-    ->set('deploy_path', '~/www/marianaespana/proyectos/typo3-project1');
+host(' @ ')
+    ->set('deploy_path', '~/www/... /.../typo3-project1');
 
 //DocumentRoot / WebRoot for the TYPO3 automaticInstallation
 set('typo3_webroot', 'public');
